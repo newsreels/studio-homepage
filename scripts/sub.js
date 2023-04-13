6d86d00f0170b08b79fdc3a1ac243d63b73e3fa7
David samir <david.samir.wd@gmail.com>
2023-04-13 10:59:54 +0400

    fix homepage

--- scripts/sub.js
+++ scripts/sub.js
@@ -1,29 +1,32 @@
+window.onload = () => {
 
-if (document.querySelector('.hNFq9')) {
-    document.querySelectorAll('.hNFq9').forEach((button) => {
-        button.addEventListener('click', (a) => {
-            let t = a.target.closest('.hNFq9')
-            console.log('target',t)
-            if (t.hasAttribute('data-focus')) {
-                t.removeAttribute('data-focus');
+    if (document.querySelector('.hNFq9')) {
+        document.querySelectorAll('.hNFq9').forEach((button) => {
+            button.addEventListener('click', (a) => {
+                let t = a.target.closest('.hNFq9')
+                console.log('target', t)
+                if (t.hasAttribute('data-focus')) {
+                    t.removeAttribute('data-focus');
+                } else {
+                    t.setAttribute('data-focus', 'open');
+                }
+            });
+
+
+
+        })
+    }
+
+    if (document.querySelector('.downloadLink')) {
+        document.querySelectorAll('.downloadLink').forEach((button) => {
+            const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+            if (isIOS) {
+                button.href = 'https://news.bullets.app/APWq/siteios';
             } else {
-                t.setAttribute('data-focus', 'open');
+                button.href = 'https://news.bullets.app/nOFn/siteand';
             }
-        });
+        })
+    }
 
 
-
-    })
 }
-
-if (document.querySelector('.downloadLink')) {
-    document.querySelectorAll('.downloadLink').forEach((button) => {
-        const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-        if (isIOS) {
-            button.href = 'https://news.bullets.app/APWq/siteios';
-        } else {
-            button.href = 'https://news.bullets.app/nOFn/siteand';
-        }
-    })
-}
-
