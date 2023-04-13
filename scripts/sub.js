4bc5a79de42ef0f1ee746d5c0ae81c76b8c40d06
David samir <david.samir.wd@gmail.com>
2023-04-13 12:17:31 +0400

    update JS

--- scripts/sub.js
+++ scripts/sub.js
@@ -1,18 +1,18 @@
 
-document.querySelectorAll('.hNFq9').forEach((button) => {
-    button.addEventListener('click', (a) => {
-        let t = a.target.closest('.hNFq9')
-        console.log('target', t)
-        if (t.hasAttribute('data-focus')) {
-            t.removeAttribute('data-focus');
-        } else {
-            t.setAttribute('data-focus', 'open');
-        }
-    });
+// document.querySelectorAll('.hNFq9').forEach((button) => {
+//     button.addEventListener('click', (a) => {
+//         let t = a.target.closest('.hNFq9')
+//         console.log('target', t)
+//         if (t.hasAttribute('data-focus')) {
+//             t.removeAttribute('data-focus');
+//         } else {
+//             t.setAttribute('data-focus', 'open');
+//         }
+//     });
 
 
 
-})
+// })
 
 document.querySelectorAll('.downloadLink').forEach((button) => {
     const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
