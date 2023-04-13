05a8c1720149e3c59c43096fb50ef61668dc3778
David samir <david.samir.wd@gmail.com>
2023-04-13 10:37:52 +0400

    remove maps

--- _next/static/chunks/pages/[[...slug]]-67d65916f7d1f842.js
+++ _next/static/chunks/pages/[[...slug]]-67d65916f7d1f842.js
@@ -1,2 +1,1 @@
 (self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[120],{52613:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[[...slug]]",function(){return u(95281)}])}},function(n){n.O(0,[962,281,774,888,179],(function(){return _=52613,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
-//# sourceMappingURL=[[...slug]]-67d65916f7d1f842.js.map
